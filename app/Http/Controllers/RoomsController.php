45113730a573f31bb6f6c76963bd424b21aa331b
Anderson <andersongmmuller@gmail.com>
2017-11-01 21:44:15 -0200

    creating basic controllers

---
+++ app/Http/Controllers/RoomsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Room;
+
+class RoomsController extends Controller
+{
+    public function index() {
+        $rooms = Room::all();
+        return view('rooms.index', compact($rooms));
+    }
+    
+    public function show(Room $room) {
+        return view('rooms.show', compact($room));
+    }
+}
